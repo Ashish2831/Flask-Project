ace22ed0c5b7d181ef32b7e01a029799f4527d9f
Ashish Patil <ashishpatil28032001@gmail.com>
2021-05-08 16:45:31 +0530

    Database Tables Created using Python Class

---
+++ models.py
@@ -0,0 +1,20 @@
+from Mysite import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    notes = db.relationship('Note')
+    
