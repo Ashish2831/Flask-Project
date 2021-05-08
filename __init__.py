bb60ea03aaa875c852277579a868bdfb8e3a231a
Ashish Patil <ashishpatil28032001@gmail.com>
2021-05-08 16:48:30 +0530

    Indicates Sub-Package of Python

---
+++ __init__.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import os
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'badjagjdgagdhadhg'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    from .models import User, Note
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.Login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    return app
+
+def create_database(app):
+    if not os.path.exists('Mysite/' + DB_NAME):
+        db.create_all(app=app)
